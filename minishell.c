5d5ab73518743fed54c9c79b7d28b9d3f4d4334f
Firas Lafi <flafi@3-G-6.42heilbronn.de>
2023-12-04 18:22:41 +0100

    working on prompt and loop

--- minishell.c
+++ minishell.c
@@ -6,14 +6,33 @@
 /*   By: flafi <flafi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 16:19:21 by flafi             #+#    #+#             */
-/*   Updated: 2023/12/03 17:41:02 by flafi            ###   ########.fr       */
+/*   Updated: 2023/12/04 18:15:00 by flafi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./minishell.h"
-
+// working on a prompt and loop
+// void ()
 int main(int argc, char **argv, char **env)
 {
+    // (void)argc;
+    (void)argv;
+    (void)env;
     
+    if (argc == 1)
+    {
+        printf(ANSI_COLOR_GREEN "MyShell> " ANSI_COLOR_RESET);
+        while(1)
+        {
+            // exit(1);
+        }
+    }
+    else
+    {
+        printf("error\n");    
+    }
+    // printf("PATH : %s\n", getenv("PATH"));
+    // printf("HOME : %s\n", getenv("HOME"));
+    // printf("ROOT : %s\n", getenv("ROOT"));
     return(0);
 }
--- minishell.h
+++ minishell.h
@@ -6,15 +6,22 @@
 /*   By: flafi <flafi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 16:19:24 by flafi             #+#    #+#             */
-/*   Updated: 2023/12/03 17:42:43 by flafi            ###   ########.fr       */
+/*   Updated: 2023/12/04 18:13:24 by flafi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <stdlib.h>
 # include <stdio.h>
+# include <stdlib.h>
+
+// ANSI color escape codes
+# define ANSI_COLOR_RED "\x1b[31m"
+# define ANSI_COLOR_GREEN "\x1b[32m"
+# define ANSI_COLOR_YELLOW "\x1b[33m"
+# define ANSI_COLOR_BLUE "\x1b[34m"
+# define ANSI_COLOR_RESET "\x1b[0m"
 
 typedef struct mem_block
 {
