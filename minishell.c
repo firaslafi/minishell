c81ec2142de4150dff57120e4c8db56aca2e3d6d
Mohyddine Belhaj alaya <mbelhaj-@2-A-1.42heilbronn.de>
2023-12-10 18:58:08 +0100

    LIBFT

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: mbelhaj- <mbelhaj-@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 16:19:21 by flafi             #+#    #+#             */
-/*   Updated: 2023/12/10 03:50:40 by mbelhaj-         ###   ########.fr       */
+/*   Updated: 2023/12/10 18:57:59 by mbelhaj-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,5 @@
 
 int	main(int argc, char **argv)
 {
-	int		i;
-	int		j;
-	int		j;
-	char	currentChar;
-
-	i = 1;
-	j = 0;
-	while (i < argc)
-	{
-		j = 0;
-		while (argv[i][j] != '\0')
-		{
-			currentChar = argv[i][j];
-			
-			++j;
-		}
-		++i;
-	}
+	return(0);
 }
