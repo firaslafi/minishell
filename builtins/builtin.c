f87dd65660576dc41acae02f00793624e03c0637
Firas Lafi <flafi@1-E-27.42heilbronn.de>
2023-12-27 02:29:50 +0100

    MOHEDDINE

--- builtins/builtin.c
+++ builtins/builtin.c
@@ -6,153 +6,152 @@
 /*   By: flafi <flafi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 21:56:08 by flafi             #+#    #+#             */
-/*   Updated: 2023/12/23 18:43:15 by flafi            ###   ########.fr       */
+/*   Updated: 2023/12/26 21:29:57 by flafi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-// move to the echo file
-// temp echo not finished
-//  work on the $variables
-
 // removes quotes " and ' from a string
-void remove_quotes(char *str)
+void	remove_quotes(char *str)
 {
-    int i = 0;
-    int j = 0;
-    while (str[i]) 
-    {
-        if (str[i] != '\"' && str[i] != '\'')
-        {
-            str[j] = str[i];
-            j++;
-        }
-        i++;
-    }
-    str[j] = '\0';
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] != '\"' && str[i] != '\'')
+		{
+			str[j] = str[i];
+			j++;
+		}
+		i++;
+	}
+	str[j] = '\0';
 }
 
-void ft_echo(char **cmd)
+void	ft_echo(char **cmd)
 {
-    int n_flag;
-    int i;
-    
-    i = 1;
-    n_flag = 0;
-    if (!cmd[1])
-    {
-        write(1, "\n", 1);
-        return;
-    }
-    if (cmd[1][0] == '-' && cmd[1][1] == 'n' && cmd[1][2] == '\0')
-    {
-        n_flag = 1;
-        i = 2;
-    }
-    int printed = 0; // To track whether anything has been printed
-    while (cmd[i] != NULL) 
-    {
-        remove_quotes(cmd[i]);
-        write(1, cmd[i], ft_strlen(cmd[i]));
-        printed = 1;
-        if (cmd[i + 1] != NULL) 
-            write(1, " ", 1); // Print a space between arguments
-        i++;
-    }
-    if (!n_flag && printed) // Add a newline if not -n and something was printed
-        write(1, "\n", 1);
+	int	n_flag;
+	int	i;
+	int printed;
 
+	i = 1;
+	n_flag = 0;
+	if (!cmd[1])
+	{
+		write(1, "\n", 1);
+		return ;
+	}
+	if (cmd[1][0] == '-' && cmd[1][1] == 'n' && cmd[1][2] == '\0')
+	{
+		n_flag = 1;
+		i = 2;
+	}
+    printed = 0;
+	while (cmd[i] != NULL)
+	{
+		remove_quotes(cmd[i]);
+		write(1, cmd[i], ft_strlen(cmd[i]));
+		printed = 1;
+		if (cmd[i + 1] != NULL)
+			write(1, " ", 1); 
+		i++;
+	}
+	if (!n_flag && printed)
+		write(1, "\n", 1);
 }
 // priting the pwd a.k.a cwd
-void ft_pwd(char **cmd, char **env)
+void	ft_pwd(char **cmd, char **env)
 {
+	char	cwd[4096];
 
-    (void)cmd;
-    (void)env;
-
-    char cwd[4096];
-    getcwd(cwd, sizeof(cwd));
-    // ft_putendl_fd(pwd, 1);
-    ft_putendl_fd(cwd, 1);
+	(void)cmd;
+	(void)env;
+	getcwd(cwd, sizeof(cwd));
+	ft_putendl_fd(cwd, 1);
 }
+
 // get the current working directory
-char *get_cwd()
+char	*get_cwd(void)
 {
-    char cwd[4096];
-    char *path;
-    
-    path = NULL;
-    getcwd(cwd, sizeof(cwd));
-    path = cwd;
-    return (path);
+	char	cwd[4096];
+	char	*path;
+
+	path = NULL;
+	getcwd(cwd, sizeof(cwd));
+	path = cwd;
+	return (path);
 }
 //  find the post of last slash (cd)
-int last_slash_pos(const char *str)
+int	last_slash_pos(const char *str)
 {
-    int len = ft_strlen(str);
-    int position = -1; // Default position if no slash is found
-    int i = len - 1; // Start from the end of the string
-    
-    while (i >= 0) 
-    {
-        if (str[i] == '/') {
-            position = i;
-            break;
-        }
-        i--;
-    }
-    return position;
+	int	len;
+
+	len = ft_strlen(str);
+	int position = -1;
+	int i = len - 1;
+	while (i >= 0)
+	{
+		if (str[i] == '/')
+		{
+			position = i;
+			break ;
+		}
+		i--;
+	}
+	return (position);
 }
 // remove the last found slash (cd)
-char *rm_lastfld(char *cwd)
+char	*rm_lastfld(char *cwd)
 {
-    char *path;
-    int i;
-    
-    i = last_slash_pos(cwd);
-    path = NULL;
-    if (i != -1)
-    {
-        if (i == 0)
-            return ("/");
-        path = ft_substr(cwd, 0, i);
-    }
-    
-    return (path);
+	char	*path;
+	int		i;
+
+	i = last_slash_pos(cwd);
+	path = NULL;
+	if (i != -1)
+	{
+		if (i == 0)
+			return ("/");
+		path = ft_substr(cwd, 0, i);
+	}
+	return (path);
 }
 // obviously for changing directory
-void ft_cd(char **cmd, t_mini minish)
+void	ft_cd(char **cmd, t_mini minish)
 {
-    char *home_path;
-    char *cdto_path;
-    
-    (void)minish;
-    home_path = getenv("HOME");
-    cdto_path = NULL;
-    if(!home_path)
-        home_path = "/";
-    if (cmd[1] == NULL)
-        {
-            chdir(home_path);
-            return;
-        }
-    if (cmd[1] != NULL)
-    {
-        if (ft_strncmp(cmd[1], "-", 1) == 0)
-        {
-            cdto_path = getenv("OLDPWD");
-            if(!cdto_path)
-                ft_putendl_fd("cd: OLDPWD not set", 2);
-        }
-        else if (ft_strncmp(cmd[1], "~", 1) == 0)
-            cdto_path = home_path;
-        else if (ft_strncmp(cmd[1], "..", 2) == 0)
-            cdto_path = rm_lastfld(get_cwd());
-        else
-            cdto_path = cmd[1];
-    }
-    if (cdto_path != NULL && chdir(cdto_path) == -1)
+	char	*home_path;
+	char	*cdto_path;
+
+	(void)minish;
+	home_path = getenv("HOME");
+	cdto_path = NULL;
+	if (!home_path)
+		home_path = "/";
+	if (cmd[1] == NULL)
+	{
+		chdir(home_path);
+		return ;
+	}
+	if (cmd[1] != NULL)
+	{
+		if (ft_strncmp(cmd[1], "-", 1) == 0)
+		{
+			cdto_path = getenv("OLDPWD");
+			if (!cdto_path)
+				ft_putendl_fd("cd: OLDPWD not set", 2);
+		}
+		else if (ft_strncmp(cmd[1], "~", 1) == 0)
+			cdto_path = home_path;
+		else if (ft_strncmp(cmd[1], "..", 2) == 0)
+			cdto_path = rm_lastfld(get_cwd());
+		else
+			cdto_path = cmd[1];
+	}
+	if (cdto_path != NULL && chdir(cdto_path) == -1)
 	{
 		ft_putstr_fd("MyShell: cd: ", 2);
 		ft_putstr_fd(cdto_path, 2);
@@ -160,64 +159,59 @@ void ft_cd(char **cmd, t_mini minish)
 	}
 }
 // env for env lol
-void ft_env(t_mini minish)
+void	ft_env(t_mini minish)
 {
-    t_list *current;
-    
-    current = minish.envlst;
-    while (current != NULL) 
-    {
-        ft_putendl_fd(current->content, 1);
-        current = current->next;
-    }
-    minish.rtn_code = 0;
+	t_list	*current;
+
+	current = minish.envlst;
+	while (current != NULL)
+	{
+		ft_putendl_fd(current->content, 1);
+		current = current->next;
+	}
+	minish.rtn_code = 0;
 }
-
-
 
 //  check if it is builtin or not
-int is_builtin(char **cmd, t_mini minish)
+int	is_builtin(char **cmd, t_mini minish)
 {
-    if (ft_strncmp(cmd[0], "echo", 4) == 0)
-    {
-        ft_echo(cmd); // i need to do the env variables
-        return (0);
-    }
-    else if (ft_strncmp(cmd[0], "pwd", 3) == 0)
-        {
-            ft_pwd(cmd, minish.env);
-            return (0);
-        }
-    else if (ft_strncmp(cmd[0], "cd", 2) == 0)
-        {
-            ft_cd(cmd, minish);
-            return (0);
-        }
-    else if (ft_strncmp(cmd[0], "env", 3) == 0)
-        {
-            ft_env(minish);
-            return (0);
-        }
-    else if (ft_strncmp(cmd[0], "export", 6) == 0)
-        {
-            ft_export(cmd, minish);
-            return (0);
-        }
-    else if (ft_strncmp(cmd[0], "unset", 5) == 0)
-        {
-            ft_unset(cmd, minish);
-            return (0);
-        }
-    else if (ft_strncmp(cmd[0], "exit", 4) == 0)
-        exit(0); // verify the exit thingy
-    // else if (ft_strncmp(cmd[0], "history", 7) == 0)
-    //     {
-    //         printf_hst(cmd, minish);
-    //         return (0);
-    //     }
-
-    else
-        return 1;
+	if (ft_strncmp(cmd[0], "echo", 4) == 0)
+	{
+		ft_echo(cmd); // i need to do the env variables
+		return (0);
+	}
+	else if (ft_strncmp(cmd[0], "pwd", 3) == 0)
+	{
+		ft_pwd(cmd, minish.env);
+		return (0);
+	}
+	else if (ft_strncmp(cmd[0], "cd", 2) == 0)
+	{
+		ft_cd(cmd, minish);
+		return (0);
+	}
+	else if (ft_strncmp(cmd[0], "env", 3) == 0)
+	{
+		ft_env(minish);
+		return (0);
+	}
+	else if (ft_strncmp(cmd[0], "export", 6) == 0)
+	{
+		ft_export(cmd, minish);
+		return (0);
+	}
+	else if (ft_strncmp(cmd[0], "unset", 5) == 0)
+	{
+		ft_unset(cmd, minish);
+		return (0);
+	}
+	else if (ft_strncmp(cmd[0], "exit", 4) == 0)
+		exit(0); // verify the exit thingy
+	// else if (ft_strncmp(cmd[0], "history", 7) == 0)
+	//     {
+	//         printf_hst(cmd, minish);
+	//         return (0);
+	//     }
+	else
+		return (1);
 }
-
-// printf_hst
--- minishell.c
+++ minishell.c
@@ -6,24 +6,12 @@
 /*   By: flafi <flafi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 16:19:21 by flafi             #+#    #+#             */
-/*   Updated: 2023/12/24 20:59:21 by flafi            ###   ########.fr       */
+/*   Updated: 2023/12/26 21:31:23 by flafi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./minishell.h"
-// working on a prompt and loop
 
-// char **token;
-// token = history_tokenize(input);
-// add_history(input);
-// if (token[0])
-// {
-//     if (strcmp(token[0], "pwd") == 0)
-//        excute
-// }
-// exit(1);
-// envpp.env = function_duplication(env);
-// t_glob envpp;
 t_historylist	*ft_hstlast(t_historylist *lst)
 {
 	while (lst)
@@ -49,82 +37,88 @@ void	ft_addhst_back(t_historylist **history, t_historylist *new)
 			*history = new;
 	}
 }
-void add_history_front(t_historylist **history, t_historylist *newEntry)
+void	add_history_front(t_historylist **history, t_historylist *newEntry)
 {
-    // Add the entry to the front of the list
-    ft_addhst_back(history, newEntry);
-    // newEntry->next = *history;
-    // *history = newEntry;
-
+	// Add the entry to the front of the list
+	ft_addhst_back(history, newEntry);
 }
 
 // Create a new history entry & add to the front of the list
-void ft_add_history(const char *command, t_historylist **history, t_mem_block **lst)
+void	ft_add_history(const char *command, t_historylist **history,
+		t_mem_block **lst)
 {
-    // Create a new history entry
-    t_historylist *newEntry = ft_malloc(lst, sizeof(t_historylist));
-    // do a rework for strdup or add to the lst free
-    newEntry->command = ft_strdup_s(command, lst);
-    newEntry->next = NULL;
-    add_history_front(history, newEntry);
-}
-    // t_historylist *history = NULL;
-void printf_hst(t_historylist *history)
-{
-    int index = 0;
-    t_historylist *current = history;
-    while (current != NULL)
-    {
-        printf("%d %s\n", index++, current->command);
-        current = current->next;
-    }
-}
-    
-// call ft_add_history(input, &history, &lst);
-/* above is pointless*//* above is pointless*/
-/* above is pointless*//* above is pointless*/
+	t_historylist	*newEntry;
 
-char  **make_encpy(char **envp, t_mem_block *lst)
+	newEntry = ft_malloc(lst, sizeof(t_historylist));
+	// do a rework for strdup or add to the lst free
+	newEntry->command = ft_strdup_s(command, lst);
+	newEntry->next = NULL;
+	add_history_front(history, newEntry);
+}
+// t_historylist *history = NULL;
+void	printf_hst(t_historylist *history)
 {
-    int envp_size = 0;
-    while (envp[envp_size]) 
-    {
-        envp_size++;
-    }
-    char **envcpy = ft_malloc(&lst, (envp_size + 1) * sizeof(char *));
-    // Allocate memory for envcpy
-    if (envcpy == NULL)
-    {
-        printf("Memory allocation failed\n");
-        exit(1);
-    }
-    int i = 0;
-    while (envp[i]) 
-    {
-        // Allocate memory for each environment variable and copy its content
-        envcpy[i] = ft_strdup_s(envp[i], &lst);
-        i++;
-    }
-     envcpy[i] = NULL;
-     return (envcpy);
+	int				index;
+	t_historylist	*current;
+
+	index = 0;
+	current = history;
+	while (current != NULL)
+	{
+		printf("%d %s\n", index++, current->command);
+		current = current->next;
+	}
+}
+
+// call ft_add_history(input, &history, &lst);
+/* above is pointless*/ /* above is pointless*/
+/* above is pointless*/ /* above is pointless*/
+
+char	**make_encpy(char **envp, t_mem_block *lst)
+{
+	int		envp_size;
+	char	**envcpy;
+	int		i;
+
+	envp_size = 0;
+	while (envp[envp_size])
+	{
+		envp_size++;
+	}
+	envcpy = ft_malloc(&lst, (envp_size + 1) * sizeof(char *));
+	// Allocate memory for envcpy
+	if (envcpy == NULL)
+	{
+		printf("Memory allocation failed\n");
+		exit(1);
+	}
+	i = 0;
+	while (envp[i])
+	{
+		// Allocate memory for each environment variable and copy its content
+		envcpy[i] = ft_strdup_s(envp[i], &lst);
+		i++;
+	}
+	envcpy[i] = NULL;
+	return (envcpy);
 }
 // change it later to ft malloc for leaks
-t_list *fill_envlst(char **envp)
+t_list	*fill_envlst(char **envp)
 {
-    int i;
-    t_list *envlst;
-    
-    i = 1;
-    envlst = ft_lstnew(envp[0]);
-    // printf("content of the first lst = %s\n", envlst->content);
-    while(envp[i])
-    {
-        ft_lstadd_back(&envlst, ft_lstnew(envp[i]));
-        i++;
-    }
-    return (envlst);
+	int		i;
+	t_list	*envlst;
+
+	i = 1;
+	envlst = ft_lstnew(envp[0]);
+	// printf("content of the first lst = %s\n", envlst->content);
+	while (envp[i])
+	{
+		ft_lstadd_back(&envlst, ft_lstnew(envp[i]));
+		i++;
+	}
+	return (envlst);
 }
-/*pipex*//*pipex*//*pipex*//*pipex*//*pipex*//*pipex*//*pipex*/
+/*pipex*/ /*pipex*/ /*pipex*/ /*pipex*/ /*pipex*/ /*pipex*/ /*pipex*/
 // checking for excuting permissions and return path
 char	*ft_check_x(char **array, char **cmd1)
 {
@@ -146,281 +140,298 @@ char	*ft_check_x(char **array, char **cmd1)
 			i++;
 	}
 	if (!path)
-		{
-            // ft_error("no path");
-            printf("no path");
-        }
+	{
+		// ft_error("no path");
+		printf("no path");
+	}
 	return (path);
 }
-// i need to verify what kind of  data i will be getting
-// and then work on the run cmd properly then run the pipes
-// , char **envp
-// i need to accepts **args
-void run_cmd(char **cmd)
-{
-    
-    char **array = ft_split(getenv("PATH"), ':');
-    char *path = ft_check_x(array, cmd);
-	char	**args = NULL;  
-    char *argslst = ft_strdup("");
-    int i = 1;
-    while (cmd[i])
-    {
-        argslst = ft_strjoin(argslst, cmd[i]);
-        if (cmd[i + 1])
-            argslst = ft_strjoin(argslst, " ");
-        i++;
-    }
-    if (ft_strlen(argslst) > 1)
-    {
-        // printf("args =  %s\n", argslst);
-        args = ft_split(argslst, ' ');
-    }
-    else
-    {
-        // memory issue here
-        args = malloc(1 * sizeof(char *));
-        args[0] = ft_strdup("");
-    }
-    printf("path = %s\n", path);
-    printf("arglst = %s\n", argslst);
-    // fix ENVP 111!!!!
-    // this is for tesitngs
-    char *argsv[] = { "uname", "-a", NULL };
-    execve(path, argsv, NULL);
-}
-void    run_pipes(int fd[2], char **commands, int n)
-{
-    int         i;
-    int         pid;
-    int         status;
-    int         in_fd;
-    i = 0;
-    in_fd = 0;
-    while (i < n)
-    {
-        if (i < n - 1)
-            pipe(fd);
-        pid = fork();
-        if (pid == 0)
-        {
-            if (in_fd != 0)
-            {
-                dup2(in_fd, 0);
-                close(in_fd);
-            }
-            if (i < n - 1)
-            {
-                close(fd[0]);
-                dup2(fd[1], 1);
-                close(fd[1]);
-            }
-            run_cmd(commands);
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            if (in_fd != 0)
-                close(in_fd);
-            if (i < n - 1)
-            {
-                close(fd[1]);
-                in_fd = fd[0];
-            }
-            waitpid(pid, &status, 0);
-        }
-        i++;
-    }
-}
-/*pipex*//*pipex*//*pipex*//*pipex*//*pipex*//*pipex*//*pipex*/
-// sigs
-void    signal_interrupt(int sig)
-{
-    if (sig == SIGINT)
-    {
-        rl_replace_line("", 0);
-        printf("\n");
-        rl_on_new_line();
-        rl_redisplay();
-    }
-    else if (sig == SIGQUIT)
-        exit(EXIT_SUCCESS);
-}
-void    sigs(void)
-{
-    // rl_catch_signals = 0;
-    g_sign.sa_handler = signal_interrupt;
-    g_sign.sa_flags = 0;
-    sigaction(SIGINT, &g_sign, NULL);
-    sigaction(SIGQUIT, &g_sign, NULL);
-    // rl_clear_signals();
-}
-// sigs
-// pipes
-void create_pipes(int pipes[][2], int num_pipes) 
-{
-    int i = 0;
-    while (i < num_pipes) {
-        if (pipe(pipes[i]) == -1) {
-            perror("Pipe creation failed");
-            exit(EXIT_FAILURE);
-        }
-        i++;
-    }
-}
-// pipes
-
-// to doS:
-// heredoc creating
-// void heredoc()
+// char	*check_x_abs(void)
 // {
-    
+
 // }
+void	run_cmd(char **cmd)
+{
+	char	**array;
+	char	*path;
+	char	**args;
+	char	*argslst;
+	int		i;
+	char	*argsv[];
+
+	array = ft_split(getenv("PATH"), ':');
+	args = NULL;
+	argslst = ft_strdup("");
+	i = 1;
+	if (cmd[0][0] == '/')
+	{
+		if (access(cmd[0], X_OK) == 0)
+			path = cmd[0];
+		else
+			printf("no access or invalid\n");
+	}
+	else
+	{
+		path = ft_check_x(array, cmd);
+	}
+	while (cmd[i])
+	{
+		argslst = ft_strjoin(argslst, cmd[i]);
+		if (cmd[i + 1])
+			argslst = ft_strjoin(argslst, " ");
+		i++;
+	}
+	if (ft_strlen(argslst) > 1)
+	{
+		// printf("args =  %s\n", argslst);
+		args = ft_split(argslst, ' ');
+	}
+	else
+	{
+		// memory issue here
+		args = malloc(1 * sizeof(char *));
+		args[0] = ft_strdup("");
+	}
+	// printf("path = %s\n", path);
+	// printf("arglst = %s\n", argslst);
+	// fix ENVP 111!!!!
+	// this is for tesitngs
+	//  CMD then FLAGS then NULL
+	argsv[] = {cmd[0], "-a", NULL};
+	execve(path, argsv, NULL);
+}
+void	run_pipes(int fd[2], char **commands, int n)
+{
+	int	i;
+	int	pid;
+	int	status;
+	int	in_fd;
+
+	i = 0;
+	in_fd = 0;
+	while (i < n)
+	{
+		if (i < n - 1)
+			pipe(fd);
+		pid = fork();
+		if (pid == 0)
+		{
+			if (in_fd != 0)
+			{
+				dup2(in_fd, 0);
+				close(in_fd);
+			}
+			if (i < n - 1)
+			{
+				close(fd[0]);
+				dup2(fd[1], 1);
+				close(fd[1]);
+			}
+			run_cmd(commands);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			if (in_fd != 0)
+				close(in_fd);
+			if (i < n - 1)
+			{
+				close(fd[1]);
+				in_fd = fd[0];
+			}
+			waitpid(pid, &status, 0);
+		}
+		i++;
+	}
+}
+/*pipex*/ /*pipex*/ /*pipex*/ /*pipex*/ /*pipex*/ /*pipex*/ /*pipex*/
+// sigs
+void	signal_interrupt(int sig)
+{
+	if (sig == SIGINT)
+	{
+		rl_replace_line("", 0);
+		printf("\n");
+		rl_on_new_line();
+		rl_redisplay();
+	}
+	else if (sig == SIGQUIT)
+		exit(EXIT_SUCCESS);
+}
+void	sigs(void)
+{
+	// rl_catch_signals = 0;
+	g_sign.sa_handler = signal_interrupt;
+	g_sign.sa_flags = 0;
+	sigaction(SIGINT, &g_sign, NULL);
+	sigaction(SIGQUIT, &g_sign, NULL);
+	// rl_clear_signals();
+}
+// sigs
+
+// pipes
+void	create_pipes(int pipes[][2], int num_pipes)
+{
+	int	i;
+
+	i = 0;
+	while (i < num_pipes)
+	{
+		if (pipe(pipes[i]) == -1)
+		{
+			perror("Pipe creation failed");
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
+}
 
 // strchr for token 2d
-int ft_strchr_2d(char **array)
+int	ft_strchr_2d(char **array)
 {
-    int i;
-    
-    i = 0;
-    
-    while (array[i])
-    {
-        if (ft_strchr(array[i], '$')!= NULL)
-            return (1);
-        i++;
-    }
-    return (0);
+	int	i;
+
+	i = 0;
+	while (array[i])
+	{
+		if (ft_strchr(array[i], '$') != NULL)
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-// check if exists 
-int check_exist_envp(char *var_name, t_mini minish)
+// check if exists
+int	check_exist_envp(char *var_name, t_mini minish)
 {
-    t_list *current;
-    
-    current = minish.envlst;
-    var_name++;
-    while (current)
-    {
-        if (ft_strncmp(var_name, current->content, ft_strlen(var_name)) == 0)
-            {
-                char *lol = ft_substr(current->content, ft_strlen(var_name) + 2, ft_strlen(current->content) - ft_strlen(var_name));
-                printf("%s", lol);
-                free(lol);
-                // break;
-                return (0);
-            }
-        current = current->next;
-    }
-    return (1);
+	t_list	*current;
+	char	*lol;
+
+	current = minish.envlst;
+	var_name++;
+	while (current)
+	{
+		if (ft_strncmp(var_name, current->content, ft_strlen(var_name)) == 0)
+		{
+			lol = ft_substr(current->content, ft_strlen(var_name) + 2,
+				ft_strlen(current->content) - ft_strlen(var_name));
+			printf("%s", lol);
+			free(lol);
+			return (0);
+		}
+		current = current->next;
+	}
+	return (1);
 }
 
-void handle_dolla(t_mini *minish, char **token)
+void	handle_dolla(t_mini *minish, char **token)
 {
-    int i;
-    int j;
-    (void)minish;
-    j = 0;
-    i = 0;
-    while (token[i])
-    {
-        if (ft_strchr(token[i], '$')!= NULL)
-        {
-            while(token[i][j])
-            {
-                if(token[i][j] == '$' && token[i][j + 1] && token[i][j + 1] == '$')
-                    {
-                        printf("%i", getpid());
-                        j++;
-                    }
-                else
-                    {
-                        check_exist_envp(token[i], *minish);
-                        j++;
-                    }
-                j++;
-            }
-            j = 0;
-        }
-        i++;
-    }
-    // printf("MyShell : %i\n", minish->rtn_code);
+	int	i;
+	int	j;
+
+	(void)minish;
+	j = 0;
+	i = 0;
+	while (token[i])
+	{
+		if (ft_strchr(token[i], '$') != NULL)
+		{
+			while (token[i][j])
+			{
+				if (token[i][j] == '$' && token[i][j + 1] && token[i][j
+					+ 1] == '$')
+				{
+					printf("%i", getpid());
+					j++;
+				}
+				else
+				{
+					check_exist_envp(token[i], *minish);
+					j++;
+				}
+				j++;
+			}
+			j = 0;
+		}
+		i++;
+	}
+	// printf("MyShell : %i\n", minish->rtn_code);
 }
 
 // look for env variables and change them in the print of intereption
 int	main(int argc, char **argv, char **envp)
 {
-    char *input;
-    t_mem_block *lst = NULL;
-    char **token;
-    t_historylist *history = NULL;
-    // init struct idk how many things
-    t_mini minish;
-    minish.env = envp;
-    minish.envlst = fill_envlst(envp);
-    minish.exp_flag = 0;
-    minish.rtn_code = 0;
-    
-    // system("leaks minishell");
-    (void)argv;
-    
-    if (argc != 1)
-    {
-        printf("error\n");
-        exit(1);
-    }
-    //  testing here
-    // int pipes[2][2];
-    
-    char **envcpy = make_encpy(envp, lst);
-    (void)envcpy;
-    sigs();
-    while (1) 
-    {
-        input = readline(ANSI_COLOR_GREEN"MyShell$ " ANSI_COLOR_RESET);
+	char			*input;
+	t_mem_block		*lst;
+	char			**token;
+	t_historylist	*history;
+	t_mini			minish;
+	char			**envcpy;
 
-        if (input == NULL)
-            break; // Exit the loop if input is NULL (EOF or error)
-        if (input[0] != '\0') 
-        {
-            add_history(input);
-            token = history_tokenize(input);
-            ft_add_history(input, &history, &lst);
-            free(input);
-            if (token != NULL && *token != NULL)
-            {
-
-                // printf("cmd = %s\n", *token);
-                // char *vv[] = {"", NULL};
-                // execve("/usr/bin/uname", vv, envp);
-                if (is_builtin(token, minish) == 0);
-                    
-                else if (ft_strncmp(token[0], "history", 7) == 0)
-                    printf_hst(history);
-                else if (ft_strchr_2d(token) == 1)
-                    {
-                        // fill the commands withteh code an the failed one with their appropriate 
-                        handle_dolla(&minish, token);
-                    }
-                else
-                   {
-                    // handle excution with absolute and relative path
-                    // create_pipes(pipes, 2);
-                    // run_pipes(pipes[0] ,token, 2);
-                    run_cmd(token);
-                   } 
-                
-                free(token);
-            } else if (token != NULL) {
-                free(token); // Free memory for an empty token
-            }
-        } else {
-            free(input); // Free memory for an empty input
-        }
-    }
-
-    ft_free_all(&lst);
-
-
-    return(0);
+	lst = NULL;
+	history = NULL;
+	// init struct idk how many things
+	minish.env = envp;
+	minish.envlst = fill_envlst(envp);
+	minish.exp_flag = 0;
+	minish.rtn_code = 0;
+	// system("leaks minishell");
+	(void)argv;
+	if (argc != 1)
+	{
+		printf("error\n");
+		exit(1);
+	}
+	//  testing here
+	// int pipes[2][2];
+	envcpy = make_encpy(envp, lst);
+	(void)envcpy;
+	sigs();
+	while (1)
+	{
+		input = readline(ANSI_COLOR_GREEN "MyShell$ " ANSI_COLOR_RESET);
+		if (input == NULL)
+			break ; // Exit the loop if input is NULL (EOF or error)
+		if (input[0] != '\0')
+		{
+			add_history(input);
+			token = history_tokenize(input);
+			ft_add_history(input, &history, &lst);
+			free(input);
+			if (token != NULL && *token != NULL)
+			{
+				// printf("cmd = %s\n", *token);
+				// char *vv[] = {"", NULL};
+				// execve("/usr/bin/uname", vv, envp);
+				if (is_builtin(token, minish) == 0)
+					;
+				else if (ft_strncmp(token[0], "history", 7) == 0)
+					printf_hst(history);
+				else if (ft_strchr_2d(token) == 1)
+				{
+					// temp until mo moves it
+					// fill the commands withteh code an the failed one with their appropriate
+					handle_dolla(&minish, token);
+				}
+				else
+				{
+					// handle excution with absolute and relative path
+					// create_pipes(pipes, 2);
+					// run_pipes(pipes[0] ,token, 2);
+					run_cmd(token);
+				}
+				free(token);
+			}
+			else if (token != NULL)
+			{
+				free(token); // Free memory for an empty token
+			}
+		}
+		else
+		{
+			free(input); // Free memory for an empty input
+		}
+	}
+	ft_free_all(&lst);
+	return (0);
 }
